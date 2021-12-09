b5e460c30254fe21b798481ceb8e6bc8e9cc35d0
Arnaud Borgniez <arnaud.borgniez@gmail.com>
2021-12-09 21:59:01 +0100

    - ajout du fichier d'exemple des scénarios - parsing du fichier de config et création des RPIApps (+Link) - ajout des méthodes intToBytes et bytesToInt dans la classe Utils (ça pourrait servir)

--- src/application/Terminal.java
+++ src/application/Terminal.java
@@ -6,19 +6,42 @@ import models.Link;
 import models.Node;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Terminal {
     private ParserConfig parserConfig;
-    private RPIApp apps;
+    private List<RPIApp> apps = new LinkedList();
 
-    Terminal(String filePath) throws IOException, FileFormatException {
+    public Terminal(String filePath) throws IOException, FileFormatException {
        this.parserConfig = new ParserConfig(filePath);
     }
     public void run() {
-
+        this.initApps(this.parserConfig.getNodes(), this.parserConfig.getLinks(), this.parserConfig.getEvents());
     }
     private void initApps(List<Node> nodes, List<Link> links, List<Event> events) {
+        List<RPIApp> neighbors = new ArrayList();
+
+        /**
+         * Creation of the RootDevice (id==1) and others RPIApps
+         */
+        for(Node node : nodes){
+            if (node.id == 1) {
+                apps.add(0, new RootDevice(node));
+            } else {
+                apps.add((node.id-1), new RPIApp(node));
+            }
+        }
+
+        /**
+         * Linking each neighbors of RPIApp
+         */
+        for(Link link : links){
+            RPIApp rPIAppTemp = apps.get(link.destinationId-1);
+            apps.get(link.sourceId-1).addNeighbor(rPIAppTemp);
+        }
+        System.out.println(apps);
 
     }
 }
