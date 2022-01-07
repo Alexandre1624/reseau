6df3d172ac0a6746b21934272c6fa87df05dda53
aborgniez <arnaud.borgniez@gmail.com>
2022-01-07 20:48:19 +0100

    Renommage de la classe Main en RPIManagement

--- src/RPIManagement.java
+++ src/RPIManagement.java
@@ -2,7 +2,7 @@ import application.Terminal;
 import models.FileFormatException;
 import java.io.IOException;
 
-public class Main {
+public class RPIManagement {
 
     public static void main(String[] args) throws FileFormatException, IOException, InterruptedException {
         Terminal terminal = new application.Terminal(System.getProperty("user.dir") + "/scenario1.config");
